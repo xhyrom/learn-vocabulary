0fe6cccb9d35b6457253f97905ed2f95f5db0778
Jozef Steinhübl (xHyroM) <xhyrom.hyro@gmail.com>
2023-10-22 19:05:44 +0200

    astro compress

--- packages/website/astro.config.ts
+++ packages/website/astro.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "astro/config";
 
 import serviceWorker from "astrojs-service-worker";
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), serviceWorker()],
+  integrations: [tailwind(), serviceWorker(), compress()],
 });
