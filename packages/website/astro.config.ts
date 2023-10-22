916c18df9d1f3c197d448ce1763b7b42ed00c2cb
Jozef Steinhübl (xHyroM) <xhyrom.hyro@gmail.com>
2023-10-22 10:07:03 +0200

    feat: offline support

--- packages/website/astro.config.ts
+++ packages/website/astro.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
+
+import serviceWorker from "astrojs-service-worker";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), serviceWorker()],
 });
