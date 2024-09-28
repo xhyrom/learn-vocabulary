6230a0ac1674c2f89a27a38725f914df376ee7d8
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-09-28 16:28:13 +0200

    fix: remove type module

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import svelte from "@astrojs/svelte";
-
 import preact from "@astrojs/preact";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), preact({ compat: true }), tailwind()],
+  integrations: [preact({ compat: true }), tailwind()],
 });
